746f2cdb21510a96ba5c15488f6e176aa1c935ff
EokHyun Lee <ykyahwa@gmail.com>
2016-01-16 13:39:25 +0900

    변수 추가

--- app/src/main/java/com/ykyahwa/bookbestseller/data/BookSeriesData.java
+++ app/src/main/java/com/ykyahwa/bookbestseller/data/BookSeriesData.java
@@ -4,4 +4,14 @@ package com.ykyahwa.bookbestseller.data;
  * Created by SuyoungKang on 2016. 1. 16..
  */
 public class BookSeriesData {
+    private int num1;
+    private int num2;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
 }
