235bb7f237862b1fc4b0bc370cd925fa347ea1b8
EokHyun Lee <ykyahwa@gmail.com>
2016-01-23 01:00:56 +0900

    retrofit 2  적용

--- app/src/main/java/com/ykyahwa/bookbestseller/MainActivity.java
+++ app/src/main/java/com/ykyahwa/bookbestseller/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,10 +16,13 @@ import android.widget.ListView;
 import com.ykyahwa.bookbestseller.data.BookData;
 import com.ykyahwa.bookbestseller.data.BookListData;
 import com.ykyahwa.bookbestseller.main.adapter.BookListAdapter;
-import com.ykyahwa.bookbestseller.network.NetworkAsyncTask;
 import com.ykyahwa.bookbestseller.network.NetworkListner;
+import com.ykyahwa.bookbestseller.network.NetworkRetrofit;
 
 import java.util.ArrayList;
+
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,9 +38,14 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         initialize();
-        new NetworkAsyncTask(networkListner).execute((Void[]) null);
+        //AsyncTask
+//        new NetworkAsyncTask(networkListner).execute((Void[]) null);
+
+
+        new NetworkRetrofit().request(callback);
 
     }
+
 
     private void initialize() {
         bookListView = (ListView) findViewById(R.id.MAIN_LV_BOOK_LIST);
@@ -75,15 +84,34 @@ public class MainActivity extends AppCompatActivity {
             h.post(new Runnable() {
                 @Override
                 public void run() {
-                    bookDataList.clear();
 
-                    bookDataList.addAll(data.getItem());
-                    bookListAdapter.notifyDataSetChanged();
+                    dataChange(data.getItem());
                 }
             });
 
         }
     };
+
+    private Callback callback = new Callback<BookListData>() {
+        @Override
+        public void onResponse(Response<BookListData> response) {
+            Log.d("TEST","onResponse");
+            dataChange(response.body().getItem());
+        }
+
+        @Override
+        public void onFailure(Throwable t) {
+            Log.d("TEST","onFailure");
+
+        }
+    };
+
+    private void dataChange(ArrayList<BookData> newBookDataList) {
+        bookDataList.clear();
+
+        bookDataList.addAll(newBookDataList);
+        bookListAdapter.notifyDataSetChanged();
+    }
 
     private AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
         @Override
@@ -93,4 +121,5 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     };
+
 }
---
+++ app/src/main/java/com/ykyahwa/bookbestseller/network/NetworkRetrofit.java
@@ -0,0 +1,38 @@
+package com.ykyahwa.bookbestseller.network;
+
+import com.ykyahwa.bookbestseller.data.BookListData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+
+/**
+ * Created by eokhyunlee on 2016. 1. 22..
+ */
+public class NetworkRetrofit {
+    private final String INTERPARK_API_URL = "http://book.interpark.com";
+
+    public void request(Callback<BookListData> callback) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(INTERPARK_API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        InterpartService service = retrofit.create(InterpartService.class);
+        Call<BookListData> call = service.getBestSeller();
+        call.enqueue(callback);
+    }
+
+    public interface InterpartService {
+
+
+        String BESTSELLER_QUERY = "/api/bestSeller.api?key=interpark&categoryId=100&output=json";
+
+        @GET(BESTSELLER_QUERY)
+        Call<BookListData> getBestSeller(
+//                Callback<BookListData> callback
+        );
+    }
+}
